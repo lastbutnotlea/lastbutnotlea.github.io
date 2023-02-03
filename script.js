e01aeddc4e0d097101bdbee61a707d97ca5b451c
Lea Rieger <lea.rieger@gmx.net>
2023-02-03 20:33:29 +0100

    Minor content and layout changes

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const urlParams = new URLSearchParams(window.location.search);
 const name = urlParams.get("name");
 document.getElementById("name").innerHTML = name;
+/*
+function resizeIFrameToFitContent( iFrame ) {
+    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
+
+window.addEventListener('DOMContentLoaded', function(e) {
+    var iframes = document.querySelectorAll("iframe");
+    for( var i = 0; i < iframes.length; i++) {
+        resizeIFrameToFitContent( iframes[i] );
+    }
+} );*/
+
+  function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+  }
